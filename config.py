dde2b8b5b8a951df7f8b7c957b917c60292f4459
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-03-16 16:09:14 +0100

    test2

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config(object):
-    # for development, will be changed asap 
+    # for development, will be changed asap
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
