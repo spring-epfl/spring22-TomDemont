65222aa737630e0332cabe00dcc05edd950abab7
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-03-17 19:17:30 +0100

    updates login process

--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
+from app import login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(96), index=True, unique=True)
-    password_hash = db.Column(db.String(64))
+    password_hash = db.Column(db.String(102))
     attacks = db.relationship(
         "MatchResult",
         backref="attacker",
@@ -20,8 +23,20 @@ class User(db.Model):
         lazy="dynamic",
     )
 
+    def set_password(self, password):
+        # basically hashes with random salt using PBKDF2, see https://werkzeug.palletsprojects.com/en/2.0.x/utils/#module-werkzeug.security
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return "<User {} (id: {})>".format(self.username, self.id)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class MatchResult(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,39 @@
-from flask import flash, redirect, render_template, url_for
+from flask import flash, redirect, render_template, request, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_required, login_user, logout_user
+from app.models import User, MatchResult
+from werkzeug.urls import url_parse
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    user = {"username": "Robb"}
-    matches = [
-        {
-            "attacker": {"username": "Robb"},
-            "defender": {"username": "Beepboop"},
-            "result": "1/0",
-        }
-    ]
-    return render_template("index.html", title="Home", user=user, matches=matches)
+    matches = current_user.attacks
+    return render_template("index.html", title="Home", matches=matches)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(
-            "Login requested for user {}, remember_me={}".format(
-                form.username.data, form.remember_me.data
-            )
-        )
-        return redirect(url_for("index"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        # check if no next page specified or next page is out of domain
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
