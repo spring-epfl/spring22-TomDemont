38ddee2f6c5dd1cb666d591a08fba59435c4cc6f
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-05-29 23:04:53 +0200

    better launch, install requirements and better timings

--- run-srs.sh
+++ run-srs.sh
@@ -2,15 +2,20 @@
 
 trap 'jobs -p | xargs kill' SIGINT EXIT KILL STOP QUIT
 
+source venv/bin/activate
+pip install -r requirements.txt
+
 ./run-redis.sh &
 
-sleep 1
+sleep 2.5
 
 celery -A app.celery worker --loglevel=info &
 
-sleep 1
+sleep 4
 
 flask run &
+
+sleep 2
 
 echo "Spawned PID: $(jobs -p | xargs)"
 
