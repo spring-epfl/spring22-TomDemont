32b5e31b1d01ad15265b87f33aab6384dd1c2ec5
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-02 16:25:51 +0200

    format

--- app/errors.py
+++ app/errors.py
@@ -1,4 +1,7 @@
+"""Defines flask app error handlings"""
+
 from flask import render_template
+
 from app import app, db
 
 
@@ -13,21 +16,26 @@ def internal_error(error):
     db.session.rollback()
     return render_template("500.html"), 500
 
+
 @app.errorhandler(403)
 def forbidden_error(error):
     return render_template("403.html"), 403
+
 
 @app.errorhandler(400)
 def bad_request_error(error):
     return render_template("400.html"), 400
 
+
 @app.errorhandler(503)
 def service_unavailable_error(error):
     return render_template("503.html"), 503
 
+
 @app.errorhandler(405)
 def method_not_allowed(error):
     return render_template("405.html"), 405
+
 
 @app.errorhandler(413)
 def too_large(errot):
