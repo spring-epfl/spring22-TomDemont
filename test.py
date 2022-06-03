dadc623a6b714171f8c846e05c9960b118bc3b24
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-03 21:36:17 +0200

    extends test case for model

--- test.py
+++ test.py
@@ -19,6 +19,34 @@ class UserModelCase(unittest.TestCase):
         self.assertFalse(u.check_password("beep"))
         self.assertTrue(u.check_password("hoho"))
 
+    def test_users_team(self):
+        u1 = User(username="john", email="john@example.com")
+        u2 = User(username="susan", email="susan@example.com")
+        u3 = User(username="paul", email="paul@example.com")
+
+        db.session.add_all([u1, u2, u3])
+        db.session.commit()
+
+        t1 = Team(team_name="beepboop", member1_id=u1.id, member2_id=u2.id)
+
+        db.session.add(t1)
+        db.session.commit()
+
+        self.assertEquals(u1.team(), t1)
+        self.assertEquals(u2.team(), t1)
+        self.assertTrue(u1.has_team())
+        self.assertFalse(u3.has_team())
+
+
+class TeamModelCase(unittest.TestCase):
+    def setUp(self):
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
     def test_matches(self):
         # Setup users
         u1 = User(username="john", email="john@example.com")
@@ -34,8 +62,9 @@ class UserModelCase(unittest.TestCase):
         db.session.add_all([t1, t2])
         db.session.commit()
 
-        self.assertEqual(u1.team(), t1)
         self.assertEqual(t2.members(), (u3, u4))
+        self.assertTrue(t1.is_full())
+        self.assertFalse(t2.has_admin())
 
         # Setup defences
         d1 = Defence(defender_team_id=t1.id)
@@ -48,14 +77,18 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(t2.defences.all(), [d2])
 
         # Create matches
-        m1 = Match(defender_team_id=t1.id, attacker_team_id=t2.id)
-        m2 = Match(defender_team_id=t2.id, attacker_team_id=t1.id)
+        m1 = Match(defender_team_id=t1.id, attacker_team_id=t2.id, round=1)
+        m2 = Match(defender_team_id=t2.id, attacker_team_id=t1.id, round=1)
 
         db.session.add_all([m1, m2])
         db.session.commit()
 
         self.assertEqual(t1.defence_matches.all(), [m1])
         self.assertEqual(t2.attack_matches.all(), [m1])
+        self.assertEqual(t1.attacks().all(), [])
+        self.assertEqual(t1.teams_to_attack_in_round(1).all(), [t2])
+        self.assertEqual(t2.defence_matches_in_round(1).all(), [m2])
+        self.assertEqual(t1.get_match_against(1, 2).all(), [m2])
 
         # Create attacks
         a1 = Attack(match_id=m1.id)
